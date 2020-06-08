19b82bcc4deb8135b9c96a1c778cb7a150696d37
cui langyuan <cuilangyuan@hotmail.com>
2020-06-08 21:33:00 +0800

    replaced old data_gen sequence with til data_gen sequence

---
+++ CV Learning/CV Learning/data_cleaning.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import os
+import pickle
+import numpy as np
+import json
+
+jsonf = open(r'D:\til2020\train.json')
+data = json.load(jsonf)
+jsonf.close()
+
+img_directory = r'D:\til2020\train\train'
+train_img_path = []
+for entry in os.scandir(img_directory):
+    if (entry.path.endswith(".jpg")) and entry.is_file():
+        train_img_path.append(entry.path)
+images = []
+for i,path in enumerate(train_img_path):
+    img = Image.open(path)
+    print(path)
+    img_resized = img.resize((960,640))
+    img_id = data["images"][i]['id']
+    images.append([img_resized, img_id])
+    img.close()
+
+filename = r"D:\til2020\train.p\images.p"
+outfile = open(filename, "wb")
+pickle.dump(images, outfile)
+outfile.close()
+
+
+
--- CV Learning/CV Learning/data_gen.py
+++ CV Learning/CV Learning/data_gen.py
@@ -30,322 +30,93 @@ def iou(anchor_box, ground_truth_box, img_w, img_h):
     u_area = (xa_max-xa_min)*(ya_max-ya_min) + (xg_max-xg_min)*(yg_max-yg_min) - i_area
     return (i_area/u_area)
 
-class ImageVocSequence(Sequence):
-    def __init__(self, dataset, batch_size, augmentations, input_size=(224,224,3), iou_fn=iou):
-        self.x, self.y = zip(*dataset)
-        self.x_acc, self.y_acc = [], []
-        self.batch_size = batch_size
-        self.augment = augmentations
-        self.input_size = input_size
-        self.iou = iou_fn
-        self.anchor_box_shape = None
+class TILSequence(Sequence):
+  def __init__(self, img_folder, json_annotation_file, batch_size, augment_fn, input_size, label_encoder, preprocess_fn, testmode=False):
 
-    def __len__(self):
-        return int(np.ceil(len(self.x) / float(self.batch_size)))
-    def get_anchor_points(w,h,stride):
-        #Takes w, h of image as int or floats 
-        #takes stride as int/float
-
-        #returns p*q*2 np array containing (x,y) float coordinates of all anchor points on the image
-        #p, q are the number of anchor points along the x axis and y axis respectively
-
-        padx = int(((w*1./stride)%1)*stride)
-        pady = int(((h*1./stride)%1)*stride)
-        no_x = int(w/stride)
-        no_y = int(h/stride)
-        aps = np.zeros((no_x, no_y, 2))
-        pad_l = padx//2 + (padx%2)
-        pad_t = pady//2 + (pady%2)
-        cur_x = pad_l+stride//2
-        cur_y = pad_t+stride//2
-        for i in range(no_x):
-            for j in range(no_y):
-                aps[i,j] = np.array([cur_x, cur_y])
-                cur_y += stride
-            cur_x += stride
-            cur_y = pad_l+stride//2
-        return aps
-
-    def generate_anchor_boxes(w, h, stride, scale=[128,256,512], ratio=[0.5, 1, 2], no_exceed_bound = False):
-        #w, h, stride for get_anchor_points()
-        #scale is the pixel length of the side of a square anchor box, must be a list
-        #ratio is the width/height ratio of the anchor boxes, cannot be negative
-        #if no_exceed_bound is true, all boxes that exceeds the image are not added in, leaving a 0 value at their supposed locations
-
-        #returns a p*q*(s*r)*4 array, where the anchor box is stored in the form of [x_centre, y_centre, width, height].
-        # s, r are the length of the scale and ratio lists respectively
-
-        anchor_points = get_anchor_points(w,h,stride)
-        a_shape = anchor_points.shape
-        anchor_boxes = np.zeros((a_shape[0], a_shape[1], len(ratio)*len(scale), 4))
+    self._prepare_data(img_folder, json_annotation_file)
+    self.batch_size = batch_size
+    self.augment_fn = augment_fn
+    self.input_wh = (*input_size[:2][::-1],input_size[2])
+    self.label_encoder = label_encoder
+    self.preprocess_fn = preprocess_fn
+    self.testmode = testmode
     
-        side_lengths = []
-        for s in scale:
-            for r in ratio:
-                side_lengths.append([s*np.sqrt(r),s/np.sqrt(r)])
-        for i in range(a_shape[0]):
-            for j in range(a_shape[1]):
-                for k, sl in enumerate(side_lengths) :
-                    a_box = np.array([anchor_points[i,j][0], anchor_points[i,j][1], sl[0], sl[1]])
-                    if(no_exceed_bound):
-                        if(a_box[0] - a_box[2]/2<0 or a_box[0] + a_box[2]/2>w):
-                            continue
-                        elif(a_box[1] - a_box[3]/2<0 or a_box[1] + a_box[3]/2>h):
-                            continue
-                        else:
-                            anchor_boxes[i,j,k] = a_box
-                    else:
-                        anchor_boxes[i,j,k] = a_box
-        return anchor_boxes
+  def _prepare_data(self, img_folder, json_annotation_file):
+    imgs_dict = {im.split('.')[0]:im for im in os.listdir(img_folder) if im.endswith('.jpg')}
+    data_dict = {}
+    with open(json_annotation_file, 'r') as f:
+      annotations_dict = json.load(f)
+    annotations_list = annotations_dict['annotations']
+    for annotation in annotations_list:
+      img_id = str(annotation['image_id'])
+      c = annotation['category_id'] - 1 # TODO: make sure that category ids start from 1, not 0
+      boxleft,boxtop,boxwidth,boxheight = annotation['bbox']
+      if img_id in imgs_dict:
+        img_fp = os.path.join(img_folder, imgs_dict[img_id])
+        imwidth,imheight = PIL.Image.open(img_fp).size
+        if img_id not in data_dict:
+          data_dict[img_id] = []
+        box_cenx = boxleft + boxwidth/2.
+        box_ceny = boxtop + boxheight/2.
+        x,y,w,h = box_cenx/imwidth, box_ceny/imheight, boxwidth/imwidth, boxheight/imheight
 
-    def preprocess_anchor_boxes(anchor_boxes):
-        #takes in an output from generate_anchor_boxes
+        data_dict[img_id].append( [c,x,y,w,h] )
+    self.x, self.y, self.ids = [], [], []
+    for img_id, labels in data_dict.items():
+      self.x.append( os.path.join(img_folder, imgs_dict[img_id]) )
+      self.y.append( np.array(labels) )
+      self.ids.append( img_id )
 
-        #returns a n*4 array containing x,y,w,h of anchor boxes. Order is anchor box at 0,0 0,1 0,2 ... 0,q, 1,0, 1,1 ... p,q
-        arr_shape = anchor_boxes.shape
-        arr_flat = anchor_boxes.flatten()
-        temp_arr = []
-        arr_res = []
-        for v in range(len(arr_flat)/4):
-            temp_arr = [arr_flat[4*(v)], arr_flat[4*(v)+1], arr_flat[4*(v)+2], arr_flat[4*(v)+3]]
-            arr_res.append(temp_arr)
-        arr_res_np = np.array(arr_res)
+  def __len__(self):
+    return int(np.ceil(len(self.x) / float(self.batch_size)))
+  
+  def __getitem__(self, idx):
+    return self.get_batch_test(idx) if self.testmode else self.get_batch(idx)
 
-        return arr_res_np, arr_shape
-        
+  def get_batch_test(self, idx):
+    batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+    batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+    batch_ids = self.ids[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+    x_acc, y_acc = [], {}
+    original_img_dims = []
+    with Pool(self.batch_size) as p:
+      # Read in the PIL objects from filepaths
+      batch_x = p.map(load_img, batch_x)
     
-    def unflatten_anchor_boxes_arr(flat_arr, arr_shape):
-        #Inverse of preprocess_anchor_boxes
-        #array shape should be [p, q, sr, 4]
+    for x,y in zip( batch_x, batch_y ):
+      W,H = x.size
+      original_img_dims.append( (W,H) )
+      x_aug, y_aug = self.augment_fn( x, y )
+      if x_aug.size != self.input_wh[:2]:
+        x_aug.resize( self.input_wh )
+      x_acc.append( np.array(x_aug) )
+      y_dict = self.label_encoder( y_aug )
+      for dimkey, label in y_dict.items():
+        if dimkey not in y_acc:
+          y_acc[dimkey] = []
+        y_acc[dimkey].append( label )
 
-        #returns a p*q*(s*r)*4 array
+    return batch_ids, original_img_dims, self.preprocess_fn( np.array( x_acc ) ), { dimkey: np.array( gt_tensor ) for dimkey, gt_tensor in y_acc.items() }
 
-        arr_res = np.zeros(arr_shape)
-        arr_len = len(flat_arr)
+  def get_batch(self, idx):
+    batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+    batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
 
-        for i,arr in enumerate(flat_arr):
-            sr = i%arr_shape[2]
-            q = (i//arr_shape[2])%arr_shape[1]
-            p = i//(arr_shape[2]*arr_shape[1])
-            arr_res[p,q,sr] = arr
-
-        return arr_res
-
-    def create_ytrue_train(self, img, labels, anchor_boxes, iou_upper = 0.7, iou_lower = 0.3):
-        img_arr = np.array(img)
-        gt_box_arr = []
-        for label in labels:
-            gtclass, gtx, gty, gtw, gth = label
-            gtclass = int(gtclass)
-            #Might change depends on how label is given
-            gt_bbox = [gtx, gty, gtw, gth]
-            gt_box_arr.append(gt_bbox)
-
-        anchor_flat, ytrue_shape = preprocess_anchor_boxes(anchor_boxes)
-        iou_score = np.zeros(len(gt_box_arr), len(anchor_flat))
-        for i,gt_box in enumerate(gt_box_arr):
-            for j,anchor in enumerate(anchor_flat):
-                if (anchor[2] == 0):
-                    iou_score[i,j] = -1
-                else:
-                    iou_score[i,j] = self.iou(anchor, gt_box,img_arr.shape[1],img_arr.shape[0])
-        #iou_score is a l*a array where l is the number of labels and a is the number of anchor boxes. 
-        #It contains the iou score of every gt box against anchor box
-        
-    #pruning via non max suppression
-    def xywh_to_xyxy(box):
-        x, y, w, h = box
-        x_min = x-w/2.0
-        x_max = x+w/2.0
-        y_min = y-h/2.0
-        y_max = y+h/2.0
-        return (x_min,y_min,x_max,y_max)
-
-    # Malisiewicz et al.
-    def non_max_suppression_fast(boxes, iou, overlapThresh):
-        #boxes is an array of shape n*4 where n is the number of boxes and each box is in the form of (x1,y1,x2,y2)
-        #iou is the score of all the boxes against a gt box
-
-        #returns a 1D array containing all the indices of the boxes chosen
-
-        # if there are no boxes, return an empty list
-        if len(boxes) == 0:
-            return []
-
-        # if the bounding boxes integers, convert them to floats --
-        # this is important since we'll be doing a bunch of divisions
-        if boxes.dtype.kind == "i":
-            boxes = boxes.astype("float")
-
-        # initialize the list of picked indexes	
-        pick = []
-
-        # grab the coordinates of the bounding boxes
-        x1 = boxes[:,0]
-        y1 = boxes[:,1]
-        x2 = boxes[:,2]
-        y2 = boxes[:,3]
-
-        # compute the area of the bounding boxes and sort the bounding
-        # boxes by the bottom-right y-coordinate of the bounding box
-        area = (x2 - x1 + 1) * (y2 - y1 + 1)
-        idxs = np.argsort(y2)
-
-        # keep looping while some indexes still remain in the indexes
-        # list
-        while len(idxs) > 0:
-            # grab the last index in the indexes list and add the
-            # index value to the list of picked indexes
-            last = len(idxs) - 1
-            i = idxs[last]
-
-
-            # find the largest (x, y) coordinates for the start of
-            # the bounding box and the smallest (x, y) coordinates
-            # for the end of the bounding box
-            xx1 = np.maximum(x1[i], x1[idxs[:last]])
-            yy1 = np.maximum(y1[i], y1[idxs[:last]])
-            xx2 = np.minimum(x2[i], x2[idxs[:last]])
-            yy2 = np.minimum(y2[i], y2[idxs[:last]])
-
-            # compute the width and height of the bounding box
-            w = np.maximum(0, xx2 - xx1 + 1)
-            h = np.maximum(0, yy2 - yy1 + 1)
-
-            # compute the ratio of overlap
-            overlap = (w * h) / area[idxs[:last]]
-            overlap_box_indices = idxs[np.where(overlap > overlapThresh)[0]]
-            if len(overlap_box_indices) > 0:
-                i = overlap_box_indices[np.argmax(iou[overlap_box_indices])]
-            pick.append(i)
-            #iou_scores
-        
-
-            # delete all indexes from the index list that have
-            idxs = np.delete(idxs, np.concatenate(([last],
-	            np.where(overlap > overlapThresh)[0])))
-
-	    # return only the bounding boxes that were picked using the
-	    # integer data type
-        return pick
-
-    def iou_sampling(pruned_anchor_box_indices, iou_scores, iou_upper=0.7, iou_lower=0.3):
-        #pruned_anchor_box_indices is the index of all anchor boxes
-        #iou_scores is the unpruned iou_scores
-        #iou_upper, iou_lower are floats between 1 and 0 representing the iou threshold for positive cases and negative cases
-
-        #returns 2 arrays containing 256 entries.
-        #pos_res is in the shape p*2, where each entry is in the form of (i, j) where i is the index of the anchor box chosen and j is the corresponding bounding box
-        #neg_res is in the shape n where each entry is the index of the anchor box chosen
-
-        #This part gets the positives
-        pos_res = []
-        pos_case_indices = []
-        candidate_indices = pruned_anchor_box_indices
-        indices_to_del = []
-        for i,_ in enumerate(iou_scores):
-            idx = np.argmax(iou_scores[i])
-            pos_res.append([idx,i])
-            pos_case_indices.append(idx)
-            candidate_indices = candidate_indices[candidate_indices != idx]
-            for f,j in enumerate(candidate_indices):
-                if iou_scores[i,j] > iou_upper:
-                    pos_res.append([j,i])
-                    indices_to_del.append(f)
-                    pos_case_indices.append(j)
-            candidate_indices = np.delete(candidate_indices, indices_to_del)
-            indices_to_del = []
-
-        pos_res = np.array(pos_res)
-        if pos_res.shape[0] > 128:
-            pos_res = np.random.choice(pos_res, size=128, replace = False)
-        pos_count = pos_res.shape[0]
-
-        #This part gets the negatives
-        negative_candidate_indices = [i for i in range(iou_scores.shape[1]) if i not in pos_case_indices ]
-        neg_candidates = [i for i in negative_candidate_indices if np.all(iou_scores[:,i]<iou_lower) and np.all(iou_scores[:,i]>=0)]
-        neg_res = []
-        if len(neg_candidates) <= 256-pos_count:
-            neg_res = neg_candidates
-        else:
-            neg_res = np.random.choice(neg_candidates, size=256-pos_count, replace = False)
-        return pos_res, neg_res
-
-    def prune_a_box(self, anchor_boxes_flat, iou, overlapThresh = 0.5):
-        anchor_box_indices = np.array(anchor_boxes_flat.shape[0])
-        pos_box_indices, neg_box_indices = self.iou_sampling(anchor_box_indices, iou, iou_upper=0.7, iou_lower=0.3)
-
-
-
-    #def convert_labels_cxywh_to_arrays(self, labels, iou_threshold=0.5, exceed_thresh_positive=True):
-    #    num_entries = 7 # objectness, p_cat, p_dog, dx, dy, dw, dh
-    #    kx,ky = self.dims
-    #    labels_arr = np.zeros( (kx, ky, num_entries) ) # For this basic model, this is of shape (3,3,7)
-
-    #    for label in labels:
-    #    # Retrieve the ground-truth class label and bbox
-    #        gtclass, gtx, gty, gtw, gth = label
-    #        gtclass = int(gtclass)
-    #        gt_bbox = [gtx, gty, gtw, gth]
-      
-    #    iou_scores = []
-
-    #    '''
-    #    There are kx x ky cells. In the basic model, this is 3x3.
-    #    Each cell is of width=gapx and height=gapy
-    #    For the (i,j)-th tile, center-x = (0.5+i)*gapx | center-y = (0.5+j)*gapy
-    #    '''
-    #    gapx = 1.0 / kx
-    #    gapy = 1.0 / ky
-    #    # In this loop, we run through all cells of the 3x3 grid, compute the intersection-over-union w the ground-truth and also the targets to predict.
-    #    for i in range(kx):
-    #        for j in range(ky):
-    #            x = (0.5+i)*gapx
-    #            y = (0.5+j)*gapy
-
-    #            # These are fixed to the width and height of the square-cell at the moment. However, if we want more anchor boxes of varying aspect ratios, this is the place to change it.
-    #            w = gapx
-    #            h = gapy
-    #            candidate_bbox = [x,y,w,h]
-
-    #            # Based on the SSD training regime. These are the targets we wish for the CNN to predict at the end.
-    #            # Read the SSD paper: https://arxiv.org/pdf/1512.02325.pdf, for more details.
-    #            dx = (gtx - x) / w 
-    #            dy = (gty - y) / h
-    #            dw = log( gtw / w )
-    #            dh = log( gth / h )
-
-    #            IoU = self.iou( candidate_bbox, gt_bbox )
-    #            iou_scores.append( (IoU, i, j, dx, dy, dw, dh) )
-    #    # Sort by IoU: only the highest IoU scores get included into the resulting label array. Cutoff at threshold.
-    #    iou_scores.sort( key=lambda x: x[0], reverse=True )
-    #    # Count the top 25% of iou scores
-    #    top_count = max( round(len(iou_scores) * 0.25), 1)
-    #    # Remove all the grid cells that do not overlap with ground truth at all
-    #    iou_scores = [iou_score for iou_score in iou_scores if iou_score[0] > 0]
-    #    iou_scores = iou_scores[:top_count] + [iou_score for iou_score in iou_scores[top_count:] if iou_score[0] >= iou_threshold]
-    #    # Always take the top IoU entry
-    #    for iou_score in iou_scores:
-    #        # The top IoU score is always included
-    #        IoU, i, j, dx, dy, dw, dh = iou_score
-    #        payload = [IoU, 0, 0, dx,dy,dw,dh]
-    #        payload[gtclass + 1] = 1
-    #        labels_arr[i,j,:] = payload
-    #    return labels_arr
+    x_acc, y_acc = [], {}
+    with Pool(self.batch_size) as p:
+      # Read in the PIL objects from filepaths
+      batch_x = p.map(load_img, batch_x)
     
-    def preprocess_npimg(self, x):
-          return x * 1./255.
+    for x,y in zip( batch_x, batch_y ):
+      x_aug, y_aug = self.augment_fn( x, y )
+      if x_aug.size != self.input_wh[:2]:
+        x_aug.resize( self.input_wh )
+      x_acc.append( np.array(x_aug) )
+      y_dict = self.label_encoder( y_aug )
+      for dimkey, label in y_dict.items():
+        if dimkey not in y_acc:
+          y_acc[dimkey] = []
+        y_acc[dimkey].append( label )
 
-    def __getitem__(self, idx):
-        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
-        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
-
-        self.x_acc.clear()
-        self.y_acc.clear()
-        for x,y in zip( batch_x, batch_y ):
-            x_aug, y_aug = self.augment( x, y )
-            self.x_acc.append( x_aug if x_aug.shape == self.input_size else resize( x_aug, self.input_size[:2] ) )
-            y_arr = self.convert_labels_cxywh_to_arrays( y_aug )
-            self.y_acc.append( y_arr )
-
-        return self.preprocess_npimg( np.array( self.x_acc ) ), np.array( self.y_acc )
+    return self.preprocess_fn( np.array( x_acc ) ), { dimkey: np.array( gt_tensor ) for dimkey, gt_tensor in y_acc.items() }
