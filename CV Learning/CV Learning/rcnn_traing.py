0c536febca176503a971343c142622fc9cb5011c
cui langyuan <cuilangyuan@hotmail.com>
2020-06-12 17:12:20 +0800

    Moved roi pooling layer to roi_pooling

--- CV Learning/CV Learning/rcnn_traing.py
+++ CV Learning/CV Learning/rcnn_traing.py
@@ -3,7 +3,102 @@ import rpn_training
 import data_gen
 import numpy as np
 
-def get_rp(ypred, anchor_boxes, scale, ratio, max_boxes = -1, score_thresh = 0.7):
+def xywhs_to_xyxys(box):
+    #only used for NMS pruning
+    x, y, w, h, score = box
+    x_min = x-w/2.0
+    x_max = x+w/2.0
+    y_min = y-h/2.0
+    y_max = y+h/2.0
+    return np.array([x_min,y_min,x_max,y_max,score])
+
+def xyxys_to_xywhs(box):
+    #only used for NMS pruning
+    x1, y1, x2, y2, score = box
+    x = (x1+x2)/2.0
+    y = (y1+y2)/2.0
+    w = np.absolute(x1-x2)
+    h = np.absolute(y1-y2)
+    return np.array([x,y,w,h,score])
+
+# Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+    #boxes is an array of shape n*5 where n is the number of boxes and each box is in the form of (x,y,w,h,s)
+
+    #returns a p*5 array containing all the boxes of the boxes picked in the form of (x,y,w,h,s)
+
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+    #converts from xywhs to xyxys
+    for i in range(boxes.shape[0]):
+        boxes[i] = xywhs_to_xyxys(boxes[i])
+
+    #sorts the array based on score
+    scores = boxes[:,4]
+    sorted_index = np.argsort(scores)
+    boxes_sorted = []
+    for i in sorted_index:
+        boxes_sorted.append(boxes[i])
+    boxes_sorted = np.array(boxes_sorted)
+
+    # initialize the list of picked indexes	
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes_sorted[:,0]
+    y1 = boxes_sorted[:,1]
+    x2 = boxes_sorted[:,2]
+    y2 = boxes_sorted[:,3]
+
+    area = (x2 - x1 ) * (y2 - y1)
+    idxs = np.array([i for i in range(len(y2))])
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1)
+        h = np.maximum(0, yy2 - yy1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+        overlap_box_indices = idxs[np.where(overlap > overlapThresh)[0]]
+        pick.append(i)
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+	        np.where(overlap > overlapThresh)[0])))
+
+	# return only the bounding boxes that were picked using the
+	# integer data type
+    picked_boxes = []
+    for p in pick:
+        picked_boxes.append(boxes_sorted[p])
+    #converts back to xywhs form
+    for i in range(np.array(picked_boxes).shape[0]):
+        picked_boxes[i] = xyxys_to_xywhs(picked_boxes[i])
+    return np.array(picked_boxes)
+
+def get_rp(ypred, anchor_boxes, max_boxes = -1, score_thresh = 0.7):
     #ypred is a flattened depth first. so it is h1w1s1r1, h1w1s1r2...h1w2s1r1, h1w2s1r2 ... h2w1s1r1 ...
     #but the way we store anchorboxes is by width first, then height
 
@@ -13,9 +108,9 @@ def get_rp(ypred, anchor_boxes, scale, ratio, max_boxes = -1, score_thresh = 0.7
     ypred_regr = ypred[:,2:]
     for i in range(ypred.shape[0]):
         if ypred_class[i,0] > 0.7:
-            q = i // 60*9
+            q = i // (60*9)
             sr = i % 9
-            p = (i // sr) % 60
+            p = (i // 9) % 60
             box = anchor_boxes[p,q,sr]
             xa = box[0]
             ya = box[1]
@@ -32,7 +127,7 @@ def get_rp(ypred, anchor_boxes, scale, ratio, max_boxes = -1, score_thresh = 0.7
             score = ypred_class[i,0]
             proposed_region.append([x,y,w,h,score])
 
-    proposed_region = data_gen.non_max_suppression_fast(proposed_region, overlapThresh = 0.5)
+    proposed_region = non_max_suppression_fast(np.array(proposed_region), overlapThresh = 0.5)
     if max_boxes > 0:
         return proposed_region[:max_boxes]
     else:
@@ -92,221 +187,18 @@ def get_proposal_target(proposed_region, gt_boxes, N = 16, fg_iou_thresh = 0.5, 
         class_targets[N+i, 0] = 1
         selected_regions.append(proposed_region[background_regions[N-i-1]][:4])
 
-    return np.array(selected_regions), regression_targets, regregression_mask, class_targets 
+    return np.array(selected_regions), regression_targets, regression_mask, class_targets 
+
+
+#Source : https://gist.github.com/Jsevillamol/0daac5a6001843942f91f2a3daea27a7
 
 model_path = ""
 rpn = tf.keras.models.load_model(load_model_path, custom_objects={'custom_loss':rpn_training.custom_loss})
 
-
-#Source : https://gist.github.com/Jsevillamol/0daac5a6001843942f91f2a3daea27a7
-class ROIPoolingLayer(Layer):
-
-    """ Implements Region Of Interest Max Pooling 
-
-        for channel-first images and relative bounding box coordinates
-
-        
-
-        # Constructor parameters
-
-            pooled_height, pooled_width (int) -- 
-
-              specify height and width of layer outputs
-
-        
-
-        Shape of inputs
-
-            [(batch_size, pooled_height, pooled_width, n_channels),
-
-             (batch_size, num_rois, 4)]
-
-           
-
-        Shape of output
-
-            (batch_size, num_rois, pooled_height, pooled_width, n_channels)
-
-    
-
-    """
-
-    def __init__(self, pooled_height, pooled_width, **kwargs):
-
-        self.pooled_height = pooled_height
-
-        self.pooled_width = pooled_width
-
-        
-
-        super(ROIPoolingLayer, self).__init__(**kwargs)
-
-        
-
-    def compute_output_shape(self, input_shape):
-
-        """ Returns the shape of the ROI Layer output
-
-        """
-
-        feature_map_shape, rois_shape = input_shape
-
-        assert feature_map_shape[0] == rois_shape[0]
-
-        batch_size = feature_map_shape[0]
-
-        n_rois = rois_shape[1]
-
-        n_channels = feature_map_shape[3]
-
-        return (batch_size, n_rois, self.pooled_height, 
-
-                self.pooled_width, n_channels)
-
-
-
-    def call(self, x):
-
-        """ Maps the input tensor of the ROI layer to its output
-
-        
-
-            # Parameters
-
-                x[0] -- Convolutional feature map tensor,
-
-                        shape (batch_size, pooled_height, pooled_width, n_channels)
-
-                x[1] -- Tensor of region of interests from candidate bounding boxes,
-
-                        shape (batch_size, num_rois, 4)
-
-                        Each region of interest is defined by four relative 
-
-                        coordinates (x_min, y_min, x_max, y_max) between 0 and 1
-
-
-
-            # Output
-
-                pooled_areas -- Tensor with the pooled region of interest, shape
-
-                    (batch_size, num_rois, pooled_height, pooled_width, n_channels)
-
-
-
-        """
-
-        def curried_pool_rois(x): 
-
-          return ROIPoolingLayer._pool_rois(x[0], x[1], 
-
-                                            self.pooled_height, 
-
-                                            self.pooled_width)
-
-        
-
-        pooled_areas = tf.map_fn(curried_pool_rois, x, dtype=tf.float32)
-
-
-
-        return pooled_areas
-
-    
-
-    @staticmethod
-
-    def _pool_rois(feature_map, rois, pooled_height, pooled_width):
-
-        """ Applies ROI pooling for a single image and varios ROIs
-
-        """
-
-        def curried_pool_roi(roi): 
-
-          return ROIPoolingLayer._pool_roi(feature_map, roi, 
-
-                                           pooled_height, pooled_width)
-
-        
-
-        pooled_areas = tf.map_fn(curried_pool_roi, rois, dtype=tf.float32)
-
-        return pooled_areas
-
-    
-
-    @staticmethod
-
-    def _pool_roi(feature_map, roi, pooled_height, pooled_width):
-
-        """ Applies ROI pooling to a single image and a single region of interest
-
-        """
-
-
-
-        # Compute the region of interest        
-
-        feature_map_height = int(feature_map.shape[0])
-
-        feature_map_width  = int(feature_map.shape[1])
-
-        
-
-        h_start = tf.cast(feature_map_height * roi[0], 'int32')
-
-        w_start = tf.cast(feature_map_width  * roi[1], 'int32')
-
-        h_end   = tf.cast(feature_map_height * roi[2], 'int32')
-
-        w_end   = tf.cast(feature_map_width  * roi[3], 'int32')
-
-        
-
-        region = feature_map[h_start:h_end, w_start:w_end, :]
-
-        
-
-        # Divide the region into non overlapping areas
-
-        region_height = h_end - h_start
-
-        region_width  = w_end - w_start
-
-        h_step = tf.cast( region_height / pooled_height, 'int32')
-
-        w_step = tf.cast( region_width  / pooled_width , 'int32')
-
-        
-
-        areas = [[(
-
-                    i*h_step, 
-
-                    j*w_step, 
-
-                    (i+1)*h_step if i+1 < pooled_height else region_height, 
-
-                    (j+1)*w_step if j+1 < pooled_width else region_width
-
-                   ) 
-
-                   for j in range(pooled_width)] 
-
-                  for i in range(pooled_height)]
-
-        
-
-        # take the maximum of each area and stack the result
-
-        def pool_area(x): 
-
-          return tf.math.reduce_max(region[x[0]:x[2], x[1]:x[3], :], axis=[0,1])
-
-        
-
-        pooled_features = tf.stack([[pool_area(x) for x in row] for row in areas])
-
-        return pooled_features
+model = tf.keras.applications.vgg16.VGG16(input_shape = (640,960,3),weights="imagenet", include_top=False)
+model.layers.pop()
+model_2 = tf.keras.models.Sequential(name="VGG16")
+for layer in model.layers[:-1]:
+    model_2.add(layer)
+for layer in model_2.layers:
+    layer.trainable = False
